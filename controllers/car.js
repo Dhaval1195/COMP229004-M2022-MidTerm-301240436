280fbd49b9fd0c0d87795d7a8cc8b3de4d056dda
Dhaval1195 <dhaval31094@gmail.com>
2022-06-23 23:09:53 -0400

    commit first

--- controllers/car.js
+++ controllers/car.js
@@ -46,7 +46,13 @@ module.exports.details = (req, res, next) => {
 // Renders the Add form using the add_edit.ejs template
 module.exports.displayAddPage = (req, res, next) => {
     
-    // ADD YOUR CODE HERE        
+    // ADD YOUR CODE HERE   
+    let newCar = Car();
+
+    res.render('cars/add_edit', {
+        title: 'Add a new Car',
+        car: newCar
+    })       
 
 }
 
@@ -55,12 +61,62 @@ module.exports.processAddPage = (req, res, next) => {
 
     // ADD YOUR CODE HERE
 
+
+    let newCar = Car({
+        _id: req.body.id,
+        make: req.body.make,
+        model: req.body.model,
+        year: req.body.year,
+        kilometers: req.body.kilometers,
+        doors: req.body.doors,
+        seats: req.body.seats,
+        color: req.body.color,
+        price: req.body.price
+    });
+
+
+     
+
+
+    Car.create(newCar, (err, car) =>{
+        if(err)
+        {
+            console.log(err);
+            res.end(err);
+        }
+        else
+        {
+            // refresh the car list
+            console.log(car);
+            res.redirect('/cars/list');
+        }
+    });
+
 }
 
 // Gets a car by id and renders the Edit form using the add_edit.ejs template
 module.exports.displayEditPage = (req, res, next) => {
     
     // ADD YOUR CODE HERE
+    let id = req.params.id;
+
+    Car.findById(id, (err, itemToEdit) => {
+        if(err)
+        {
+            console.log(err)
+            res.end(err);
+        }
+        else
+        {
+            //show the edit view
+            res.render('cars/add_edit', {
+                title: 'Edit Car',
+                item: itemToEdit
+            })
+        }
+    });
+
+
 
 }
 
@@ -68,6 +124,36 @@ module.exports.displayEditPage = (req, res, next) => {
 module.exports.processEditPage = (req, res, next) => {
     
     // ADD YOUR CODE HERE
+
+
+    let id = req.params.id
+    
+    //Update Car Database Schema
+    let updatedItem = Car({
+        _id: req.body.id,
+        make: req.body.make,
+        model: req.body.model,
+        year: req.body.year,
+        kilometers: req.body.kilometers,
+        doors: req.body.doors,
+        seats: req.body.seats,
+        color: req.body.color,
+        price: req.body.price
+		
+    });
+
+    Car.updateOne({_id: id}, updatedItem, (err) => {
+        if(err)
+        {
+            console.log(err);
+            res.end(err);
+        }
+        else
+        {
+            //refresh the car list
+            res.redirect('/cars/list');
+        }
+    });
     
 }
 
@@ -75,5 +161,21 @@ module.exports.processEditPage = (req, res, next) => {
 module.exports.performDelete = (req, res, next) => {
     
     // ADD YOUR CODE HERE
+
+    let id = req.params.id
+
+    //Removing item by id
+    Car.remove({_id : id}, (err) => {
+        if (err)
+        {
+            console.log(err);
+            res.end(err);
+        }
+        else
+        {
+            //refresh the movie list
+            res.redirect('/cars/list');
+        }
+    });
 
 }
