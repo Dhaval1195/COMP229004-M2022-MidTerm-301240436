5dcccc8e2e728675c0d7b93077761d4a521ef08c
Dhaval1195 <dhaval31094@gmail.com>
2022-06-25 20:29:06 -0400

    Commit Final

--- routes/car.js
+++ routes/car.js
@@ -7,8 +7,12 @@ let carController = require('../controllers/car');
 function requireAuth(req, res, next)
 {
     // check if the user is logged in
-    
-    // ADD YOUR CODE HERE        
+    if (!req.isAuthenticated()) {
+        req.session.url = req.originalUrl;
+        return res.redirect('/users/signin');
+    } else {
+        next();
+    }      
 
 }
 
